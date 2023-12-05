cab6474b39359fedc70b1e7d327917902eea55c0
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-12-05 14:15:24 +0100

    Add express and index.ts.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.use("/", (req, res) => {
+	console.log("Root route hit")
+
+	res.send("Hello, world!")
+})
+
+app.listen(3001, () => {
+	console.log("Server listening on port 3000")
+})
